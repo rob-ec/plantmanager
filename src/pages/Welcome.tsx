2462f1cdaac2f9b06ed0612499300d400f3926f4
Robson M. Gomes <robson.mesquita56@gmail.com>
2021-04-22 21:24:44 -0300

    Add Welcome Screen

---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { SafeAreaView, Text, Image, StyleSheet } from 'react-native';
+
+import wateringImg from '../assets/watering.png';
+import colors from '../styles/colors';
+import { Button } from '../components/Button';
+
+export function Welcome() {
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title}>
+                Gerencie{'\n'}
+                suas plantas{'\n'}
+                de forma fácil.
+            </Text>
+
+            <Image source={wateringImg} style={styles.image} />
+            
+            <Text style={styles.subtitle}>
+                Não esqueça mais de regar suas plantas.{'\n'}
+                Nós cuidamos de lembrar você sempre que{'\n'}
+                precisar.
+            </Text>
+
+            <Button title=">" />
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: colors.heading,
+        marginTop: 44,
+    },
+    subtitle: {
+        textAlign: 'center',
+        fontSize: 18,
+        paddingHorizontal: 20,
+        color: colors.heading,
+    },
+    image: {
+        width: 292,
+        height: 284,
+    }
+});
